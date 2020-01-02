52fee67e48080ffe24cc85270f1175df89f9ba4d
sanisha7 <sanishareddy22@gmail.com>
2020-01-02 17:10:06 +0530

    nav created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+x="random data";
   constructor() { }
 
   ngOnInit() {
+    this.x=this.x + " new ";
+  }
+  fnclick(){    
+    this.x=this.x + " new ";
   }
 
 }
